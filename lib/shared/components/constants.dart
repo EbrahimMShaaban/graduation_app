66c0deb2e574842127031fba2f0edbcfe8b9ee62
EbrahimMShaaban <ebrahimmsh2019@gmail.com>
2023-02-03 16:59:14 +0200

    3/2/2023 ebrahim add style files

--- lib/shared/components/constants.dart
+++ lib/shared/components/constants.dart
@@ -18,3 +18,20 @@ const MaterialColor materialColor = MaterialColor(
     900: Color(0xFF006064),
   },
 );
+
+abstract class MediaQueryHelper {
+  static double sizeFromHeight(BuildContext context, double fraction,
+      {bool hasAppBar = true}) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    fraction = (hasAppBar ? (screenHeight - AppBar().preferredSize.
+    height -
+        MediaQuery.of(context).padding.top)
+        : screenHeight) /
+        fraction;
+    return fraction;
+  }
+
+  static double sizeFromWidth(BuildContext context, double fraction) {
+    return MediaQuery.of(context).size.width / fraction;
+  }
+}
